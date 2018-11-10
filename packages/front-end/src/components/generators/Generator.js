3beaa8faa5740631b8a3ebd40e2ccd423f1d7f8d
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2018-11-09 22:35:40 -0500

    tiny change

--- packages/front-end/src/components/generators/Generator.js
+++ packages/front-end/src/components/generators/Generator.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import ArtPieceRendererContainer from "../artPiece/ArtPieceRendererContainer";
 import NewContractForm from "../utility/NewContractForm";
+import styled from "styled-components";
+
+const GeneratorDiv = styled.div`
+  border: 1px solid black;
+`;
 
 class GeneratorsList extends Component {
   state = { stakeKey: null };
@@ -13,7 +18,7 @@ class GeneratorsList extends Component {
   render() {
     const stake = this.props.drizzleState.contracts.GeneratorRegistry.getGeneratorStake[this.state.stakeKey];
     return (
-      <div>
+      <GeneratorDiv>
         {stake && <span>SOUL Staked: {stake.value}</span>}
         <br />
         <span>Generator Address: {this.props.generator}</span>
@@ -21,12 +26,13 @@ class GeneratorsList extends Component {
         <NewContractForm
           contract="GeneratorRegistry"
           method="depositStake"
+          methodArgs={{ from: this.props.drizzleState.accounts[0] }}
           initialMethodArgs={[this.props.generator, "1000"]}
           hideInputs={true}
         >
           Stake 1,000 SOUL
         </NewContractForm>
-      </div>
+      </GeneratorDiv>
     );
   }
 }
--- packages/front-end/src/components/history/ArtPieceHistoryContainer.js
+++ packages/front-end/src/components/history/ArtPieceHistoryContainer.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import ArtPiece from "../artPiece/ArtPiece";
+import styled from "styled-components";
+
+const HistoryDiv = styled.div`
+  border: 1px solid black;
+`;
 class ArtPieceHistoryContainer extends Component {
   render() {
     return (
-      <div>
+      <HistoryDiv>
         Bought for: {this.props.event.returnValues[2]} ETH <br />
         <ArtPiece {...this.props} blockNum={this.props.event.returnValues[1]} />
-      </div>
+      </HistoryDiv>
     );
   }
 }
