5547c0a2800ad97f4c138ec905f2be71ae3c4319
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2019-03-18 11:10:44 -0400

    unsubscribe tokens subs when account switched

--- packages/front-end/src/redux/actionCreators/accountActions.js
+++ packages/front-end/src/redux/actionCreators/accountActions.js
@@ -26,50 +26,70 @@ export const setSoulUserRegistryApproval = approvalBalance => {
   return { type: SET_SOUL_USER_REGISTRY_APPROVAL, data: { approvalBalance } };
 };
 
-let SoulTransferSub1;
+let SoulTransferSub1, SoulTransferSub2, DaiTransferSub1, DaiTransferSub2, DaiApprovalSub1, DaiApprovalSub2, SoulApprovalSub1;
 
 export const getAccount = () => {
-  console.log("poll account 1.");
   return async function (dispatch, getState) {
-    console.log("poll account 2.");
     const web3 = await getWeb3();
     const currentBlock = await web3.eth.getBlockNumber();
     const accounts = await web3.eth.getAccounts();
 
     const prevAccount = getState().account;
     if (accounts && accounts.length > 0 && accounts[0] && prevAccount !== accounts[0]) {
-      console.log("new account.");
       const account = accounts[0];
       dispatch(setAccount(account));
+
+      if (SoulTransferSub1) {
+        SoulTransferSub1.unsubscribe();
+      }
+      if (SoulTransferSub2) {
+        SoulTransferSub2.unsubscribe();
+      }
+      if (DaiTransferSub1) {
+        DaiTransferSub1.unsubscribe();
+      }
+      if (DaiTransferSub2) {
+        DaiTransferSub2.unsubscribe();
+      }
+      if (DaiApprovalSub1) {
+        DaiApprovalSub1.unsubscribe();
+      }
+      if (DaiApprovalSub2) {
+        DaiApprovalSub2.unsubscribe();
+      }
+      if (SoulApprovalSub1) {
+        SoulApprovalSub1.unsubscribe();
+      }
+
       SoulTransferSub1 = SoulToken.events.Transfer({ filter: { from: account }, fromBlock: currentBlock }, (error, event) => {
         getSoulBalance(account, dispatch);
         getSoulUserRegistryApproval(account, dispatch);
       });
-      SoulToken.events.Transfer({ filter: { to: account }, fromBlock: currentBlock }, (error, event) => {
+      SoulTransferSub2 = SoulToken.events.Transfer({ filter: { to: account }, fromBlock: currentBlock }, (error, event) => {
         getSoulBalance(account, dispatch);
         getSoulUserRegistryApproval(account, dispatch);
       });
-      DaiToken.events.Transfer({ filter: { from: account }, fromBlock: currentBlock }, (error, event) => {
+      DaiTransferSub1 = DaiToken.events.Transfer({ filter: { from: account }, fromBlock: currentBlock }, (error, event) => {
         getDaiBalance(account, dispatch);
         getDaiUserSoulApproval(account, dispatch);
         getDaiUserArtonomousApproval(account, dispatch);
       });
-      DaiToken.events.Transfer({ filter: { to: account }, fromBlock: currentBlock }, (error, event) => {
+      DaiTransferSub2 = DaiToken.events.Transfer({ filter: { to: account }, fromBlock: currentBlock }, (error, event) => {
         getDaiBalance(account, dispatch);
       });
-      DaiToken.events.Approval(
+      DaiApprovalSub1 = DaiToken.events.Approval(
         { filter: { owner: account, spender: SoulToken._address }, fromBlock: currentBlock },
         (error, event) => {
           getDaiUserSoulApproval(account, dispatch);
         },
       );
-      DaiToken.events.Approval(
+      DaiApprovalSub2 = DaiToken.events.Approval(
         { filter: { owner: account, spender: Artonomous._address }, fromBlock: currentBlock },
         (error, event) => {
           getDaiUserArtonomousApproval(account, dispatch);
         },
       );
-      SoulToken.events.Approval(
+      SoulApprovalSub1 = SoulToken.events.Approval(
         { filter: { owner: account, spender: GeneratorRegistry._address }, fromBlock: currentBlock },
         (error, event) => {
           getSoulUserRegistryApproval(account, dispatch);
